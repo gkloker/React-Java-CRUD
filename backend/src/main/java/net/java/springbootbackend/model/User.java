40d9360bc56b591b5f5071e5bc3d53e63de52df3
Gabriel KLoker <gabrielkloker@gmail.com>
2021-06-05 13:34:22 -0300

    -
    -

---
+++ backend/src/main/java/net/java/springbootbackend/model/User.java
@@ -0,0 +1,75 @@
+package net.java.springbootbackend.model;
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "invitations")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "birthday")
+    private Date birthday;
+
+    @Column(name = "dni")
+    private int dni;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, Date birthday, int dni) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+        this.dni = dni;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+}
