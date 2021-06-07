4cd3ca804f8e50ba434b2b0f143aefa91c95b2e9
Gabriel Kloker <gabrielkloker@gmail.com>
2021-06-07 11:50:33 -0300

    -
    -

--- react-frontend/src/components/Header.js
+++ react-frontend/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="container">
         <h1>
           <Link to={'/'} className="text-light">
-            CRUD - React, Redux, REST API, Axios
+            CRUD - React & SpringBoot
           </Link>
         </h1>
       </div>
